6f77755b5846806033cc2407cb684ca52b1f86ab
Simone PriMaL Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-03-21 21:39:23 +0100

    ex1_finto

---
+++ ex1.js
@@ -0,0 +1,17 @@
+let string='';
+let count=0;
+
+while(true)
+{
+if(count<=7)    
+    {
+        string=string + '#';
+        console.log(string);
+        count++;
+    }
+    
+else
+    {
+        break;
+    }
+}
---
+++ ex2.js
@@ -0,0 +1,31 @@
+
+let pippo=0
+
+while(pippo<=100)
+{ 
+    console.log(pippo);
+    pippo=pippo + 1;
+}
+
+if(pippo%3)
+{
+    pippociao;
+}
+
+
+
+
+
+
+
+
+
+/*Scrivete un programma che usi console.log
+per stampare tutti i numeri da 1 a 100, con due eccezioni. 
+Per i numeri divisibili per 3, stampa "Fizz"invece del numero, 
+e per i numeri divisibili per 5 (e non 3), stampa "Buzz"invece.
+
+Quando hai funzionato, modifica il tuo programma per stampare "FizzBuzz"per 
+numeri che sono divisibili sia per 3 che per 5 
+(e continua a stampare "Fizz"o "Buzz"per numeri 
+divisibili solo per uno di quelli).*/
